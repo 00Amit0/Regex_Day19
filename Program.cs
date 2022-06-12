4c7c9d2781ea937568d60a38c6467debe2477860
“00Amit0” <amit836961@gmail.com>
2022-06-13 01:18:55 +0530

    Checking if firstname is matching the pattern

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Regular Expressions");
+            ValidUserRegistraton validUserRegistraton = new ValidUserRegistraton();
+            Console.WriteLine("Enter valid first name");
+            string frstname = Console.ReadLine();
+            validUserRegistraton.ValidateFName(frstname);
         }
     }
 }
